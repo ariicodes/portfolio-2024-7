d8f277b475489da3d6d606b98f2bc795d5378c9d
Ariela Israel <arielaisrael12@gmail.com>
2024-07-21 14:53:42 -0400

    Nav updated

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Nav = () => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -12,33 +12,39 @@ const Nav = () => {
 	};
 
 	return (
-		<motion.nav
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			transition={{ duration: 1 }}
-			className='justify-self-start uppercase xl:h-screen xl:w-2/12 xl:border-r-4 border-[#F364B4]/25 xl:pt-12 flex flex-col'
-		>
-			{isOpen && (
-				<motion.ul
-					initial={{ opacity: 0 }}
-					animate={{ opacity: 1 }}
-					transition={{ duration: 1 }}
-					className='flex flex-col divide-y-[.5px] divide-white/25 *:py-6 *:ml-8 border-b-4 border-[#F364B4]/25 xl:border-0 xl:hidden'
+		<nav className='justify-self-start uppercase xl:h-screen xl:w-2/12 xl:border-r-4 border-[#F364B4]/25 xl:pt-12 flex flex-col'>
+			<AnimatePresence>
+				<motion.div
+					initial={{ opacity: 0, y: -100 }}
+					animate={{ opacity: 1, y: 0 }}
+					exit={{ opacity: 0, y: -100 }}
+					transition={{ duration: 2 }}
+					className='flex flex-col xl:hidden'
 				>
-					<li>
-						<Link href='/'>Home</Link>
-					</li>
-					<li>
-						<Link href='/about'>About</Link>
-					</li>
-					<li>
-						<Link href='/contact'>Projects</Link>
-					</li>
-					<li>
-						<Link href='/contact'>Contact</Link>
-					</li>
-				</motion.ul>
-			)}
+					{isOpen && (
+						<ul className='flex flex-col divide-y-[.5px] divide-white/25 *:py-6 *:ml-8 border-b-4 border-[#F364B4]/25 xl:border-0 xl:hidden'>
+							<li>
+								<Link href='/'>Home</Link>
+							</li>
+							<li>
+								<Link href='/about'>About</Link>
+							</li>
+							<li>
+								<Link href='/projects'>Projects</Link>
+							</li>
+							<li>
+								<Link href='/contact'>Contact</Link>
+							</li>
+						</ul>
+					)}
+					<button
+						className='self-end mt-6 mr-9 text-[#00F7F4] block xl:hidden'
+						onClick={handleToggle}
+					>
+						{isOpen ? 'close' : 'menu'}
+					</button>
+				</motion.div>
+			</AnimatePresence>
 			{/* FOR LARGER SCREENS BELOW */}
 			<ul className='xl:flex flex-col divide-y-[.5px] divide-white/25 *:py-6 *:ml-8 border-b-4 border-[#F364B4]/25 xl:border-0 hidden'>
 				<li>
@@ -48,19 +54,13 @@ const Nav = () => {
 					<Link href='/about'>About</Link>
 				</li>
 				<li>
+					<Link href='/projects'>Projects</Link>
+				</li>
+				<li>
 					<Link href='/contact'>Contact</Link>
 				</li>
 			</ul>
-			<motion.button
-				whileTap={{ scale: 0.5 }}
-				animate={{ scale: 1 }}
-				transition={{ duration: 0.8 }}
-				className='self-end mt-6 mr-9 text-[#00F7F4] block xl:hidden'
-				onClick={handleToggle}
-			>
-				{isOpen ? 'close' : 'view menu'}
-			</motion.button>
-		</motion.nav>
+		</nav>
 	);
 };
 
