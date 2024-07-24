2d838b6ed4836677350efce8297623ad0203af9a
Ariela Israel <arielaisrael12@gmail.com>
2024-07-24 01:55:25 -0400

    modified title component

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,9 +1,19 @@
+'use client';
+
+import { useScroll, useTransform, motion } from 'framer-motion';
+
 const Title = ({ title, sliceNum }: { title: string; sliceNum?: number }) => {
+	const { scrollY } = useScroll();
+	const opacity = useTransform(scrollY, [0, 600], [1, 0.25]);
+
 	const titleSlice1 = title.slice(0, sliceNum);
 	const titleSlice2 = title.slice(sliceNum);
 
 	return (
-		<h1 className='bg-gray-950 w-screen xl:w-10/12 z-20 fixed right-0 bottom-0 text-[#F364B4] text-right text-8xl xl:text-[12rem] uppercase font-black xl:self-end leading-[5rem] xl:leading-[10rem] p-6'>
+		<motion.h1
+			style={{ opacity }}
+			className='z-20 fixed right-0 bottom-0 text-[#F364B4] text-right text-8xl xl:text-[12rem] uppercase font-black xl:self-end leading-[5rem] xl:leading-[10rem] p-6 bg-gradient-to-r from-transparent to-gray-950'
+		>
 			{title.length <= 5 ? (
 				title
 			) : (
@@ -13,7 +23,7 @@ const Title = ({ title, sliceNum }: { title: string; sliceNum?: number }) => {
 					{titleSlice2}
 				</>
 			)}
-		</h1>
+		</motion.h1>
 	);
 };
 
